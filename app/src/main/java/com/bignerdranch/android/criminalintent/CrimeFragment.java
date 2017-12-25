87083c9173652c6d74c74580829866281a1fe550
Bellkross <vasilenkowm@yandex.ua>
2017-12-26 01:59:33 +0200

    action dial and get number added

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -14,6 +15,7 @@ import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,11 +32,13 @@ import static android.widget.CompoundButton.OnClickListener;
 
 public class CrimeFragment extends Fragment {
 
+    private static final String LOG_TAG = "CrimeFragment";
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
 
     private static final int REQUEST_DATE = 0;
     private static final int REQUEST_CONTACT = 1;
+    private static final int REQUEST_CALL = 2;
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -43,6 +47,7 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckbox;
     private Button mReportButton;
     private Button mSuspectButton;
+    private Button mCallButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -127,7 +132,6 @@ public class CrimeFragment extends Fragment {
         mReportButton = (Button) v.findViewById(R.id.crime_report);
         mReportButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 ShareCompat.IntentBuilder ib = ShareCompat.IntentBuilder.from(getActivity());
                 ib.setType("text/plain").setText(getCrimeReport()).
                         setSubject(getString(R.string.crime_report_subject)).createChooserIntent();
@@ -155,6 +159,18 @@ public class CrimeFragment extends Fragment {
             mSuspectButton.setText(mCrime.getSuspect());
         }
 
+        final Intent call = new Intent(Intent.ACTION_DIAL);
+        mCallButton = (Button) v.findViewById(R.id.crime_call);
+        mCallButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number;
+                number = mCallButton.getText().toString();
+                call.setData(Uri.parse("tel:" + number));
+                startActivityForResult(call, REQUEST_CALL);
+            }
+        });
+
         return v;
     }
 
@@ -163,23 +179,24 @@ public class CrimeFragment extends Fragment {
         if (resultCode != Activity.RESULT_OK) {
             return;
         }
-
+        Uri contactUri;
         if (requestCode == REQUEST_DATE) {
             Date date = (Date) data
                     .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mCrime.setDate(date);
             updateDate();
         } else if (requestCode == REQUEST_CONTACT && data != null) {
-            Uri contactUri = data.getData();
+            contactUri = data.getData();
+            ContentResolver cr = getActivity().getContentResolver();
             // Определение полей, значения которых должны быть
             // возвращены запросом.
             String[] queryFields = new String[]{
-                    ContactsContract.Contacts.DISPLAY_NAME
+                    ContactsContract.Contacts.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone._ID
             };
             // Выполнение запроса - contactUri здесь выполняет функции
             // условия "where"
-            Cursor c = getActivity().getContentResolver()
-                    .query(contactUri, queryFields, null, null, null);
+            Cursor c = cr.query(contactUri, queryFields, null, null, null);
             try {
             // Проверка получения результатов
                 if (c.getCount() == 0) {
@@ -188,13 +205,34 @@ public class CrimeFragment extends Fragment {
             // Извлечение первого столбца данных - имени подозреваемого.
                 c.moveToFirst();
                 String suspect = c.getString(0);
+                String contactID = c.getString(1);
                 mCrime.setSuspect(suspect);
                 mSuspectButton.setText(suspect);
+                Cursor cursor = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        new String[]{contactID}, null);
+                String number;
+                if(cursor.moveToFirst()){
+                    number = cursor.getString(
+                            cursor.getColumnIndex(
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    Log.d(LOG_TAG,number);
+                    mCallButton.setText(number);
+                    while (cursor.moveToNext()) {
+                        Log.d(LOG_TAG,cursor.getString(
+                                cursor.getColumnIndex(
+                                        ContactsContract.CommonDataKinds.Phone.NUMBER)));
+                    }
+                }
+
+                cursor.close();
             } finally {
                 c.close();
             }
         }
     }
+
 
     private void updateDate() {
         mDateButton.setText(mCrime.getDate().toString());
